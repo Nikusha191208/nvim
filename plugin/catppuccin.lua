ba387b78b71f2a002921d3dda9cb0d015bff25f2
Nikusha191208 <Nikusha191208@gmail.com>
2025-03-13 23:25:44 +0400

    make neovim background as terminal background

--- plugin/catppuccin.lua
+++ plugin/catppuccin.lua
@@ -1,9 +1,17 @@
 require("catppuccin").setup({
+    transparent_background = true,
     integrations = {
         coc_nvim = true,
         treesitter = true,
         nvimtree = true
-    }
+    },
+    ---- black background
+    --color_overrides = {
+    --    mocha = {
+    --        base = "#000000",
+    --    },
+    --},
 })
 
 vim.cmd.colorscheme 'catppuccin'
+
