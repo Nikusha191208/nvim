7b5708d62b5d465a669ce8c040e2a706f3e9d27c
Nikusha191208 <Nikusha191208@gmail.com>
2025-03-16 00:15:36 +0400

    forgot to initialize in init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 vim.cmd('source ~/.config/nvim/settings.lua')
 vim.cmd('source ~/.config/nvim/autopairs.lua')
+vim.cmd('source ~/.config/nvim/folds.lua')
 vim.cmd('source ~/.config/nvim/window.lua')
